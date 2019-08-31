5627e28aa52716dd71c45842ca122a41d9207872
Nik <ironman_fern@comcast.net>
2019-08-31 05:01:39 -0500

    Edited name

--- src/com/cgme/Main.java
+++ src/com/cgme/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Controllers/Main/MainController.fxml"));
-        primaryStage.setTitle("EZStatement v1.0");
+        primaryStage.setTitle("EZStatements v1.0");
         primaryStage.setScene(new Scene(root, 550, 750));
         primaryStage.show();
     }
